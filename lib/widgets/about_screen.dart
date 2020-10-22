4845cb40dcc4cf95cc96d10e1423d85385f8b7f6
Renan Santos <renan.stos077@gmail.com>
2020-10-22 01:22:06 -0300

    about screen title

--- lib/widgets/about_screen.dart
+++ lib/widgets/about_screen.dart
@@ -8,18 +8,30 @@ class AboutScreen extends StatelessWidget {
         body: Column(
           children: [
             Container(
-              margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 0.0),
-              alignment: Alignment.center,
-              child: Text("Health & Wellness",
-                  style:
-                      TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)),
-            ),
+                margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 0.0),
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('lib/assets/img/logo.png'),
+                      height: 100,
+                      width: 100,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text("Health & Wellness",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24.0)),
+                  ],
+                )),
             Container(
               margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
               alignment: Alignment.center,
               child: Text(
                 "O Health and Wellness é um sistema de controle de prontuário pessoal, no qual busca dar a oportunidade de carregar consigo mesmo todo seu histórico de consulta e exame, trazendo a comodidade e liberdade de migrar de hospitais/médicos levando todos seus antecedentes de saúde.",
-                textAlign: TextAlign.justify,
+                textAlign: TextAlign.left,
                 style: TextStyle(fontSize: 18.0),
               ),
             ),
