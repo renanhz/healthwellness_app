19552c7ffded4e4ebf93a88d2e344f18d9e79321
Renan Santos <renan.stos077@gmail.com>
2020-11-24 18:16:43 -0300

    scrollable screens

--- lib/widgets/about_screen.dart
+++ lib/widgets/about_screen.dart
@@ -23,84 +23,88 @@ class AboutScreen extends StatelessWidget {
                     title: Text(S.of(context).aboutUs),
                     actions: [langButton(context)],
                   ),
-                  body: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                              margin: EdgeInsets.symmetric(
-                                  vertical: 15.0, horizontal: 0.0),
-                              alignment: Alignment.center,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image(
-                                    image:
-                                        AssetImage('lib/assets/img/logo.png'),
-                                    height: 100,
-                                    width: 100,
-                                  ),
-                                  Text(S.of(context).appName,
-                                      style: GoogleFonts.staatliches(
-                                          textStyle: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 30.0))),
-                                ],
-                              )),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 0.0),
-                            alignment: Alignment.center,
-                            child: Text(
-                              S.of(context).about,
-                              textAlign: TextAlign.justify,
-                              style: GoogleFonts.teko(
-                                  textStyle: TextStyle(
-                                      fontSize: 25.0, color: Colors.grey[600])),
-                            ),
-                          ),
-                          SizedBox(height: 20.0),
-                          FlatButton(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                            ),
-                            color: Colors.red,
-                            textColor: Colors.white,
-                            disabledColor: Colors.grey,
-                            disabledTextColor: Colors.black,
-                            padding: EdgeInsets.all(8.0),
-                            splashColor: Colors.redAccent,
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => LoginScreen()));
-                            },
-                            child: Text(
-                              S.of(context).login,
-                              style: TextStyle(fontSize: 30.0),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        alignment: Alignment.bottomCenter,
-                        child: Column(
+                  body: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
                           children: [
-                            Text(S.of(context).developedBy,
+                            Container(
+                                alignment: Alignment.center,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image(
+                                      image:
+                                          AssetImage('lib/assets/img/logo.png'),
+                                      height: 100,
+                                      width: 100,
+                                    ),
+                                    Text(S.of(context).appName,
+                                        style: GoogleFonts.staatliches(
+                                            textStyle: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 30.0))),
+                                  ],
+                                )),
+                            Container(
+                              margin:
+                                  EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 0.0),
+                              alignment: Alignment.center,
+                              child: Text(
+                                S.of(context).about,
                                 textAlign: TextAlign.justify,
                                 style: GoogleFonts.teko(
                                     textStyle: TextStyle(
-                                        color: Colors.black, fontSize: 18.0))),
-                            SizedBox(height: 1),
-                            Text(S.of(context).copyright,
-                                style: GoogleFonts.teko(
-                                    textStyle: TextStyle(
-                                        color: Colors.black, fontSize: 18.0)))
+                                        fontSize: 25.0,
+                                        color: Colors.grey[600])),
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            FlatButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                              color: Colors.red,
+                              textColor: Colors.white,
+                              disabledColor: Colors.grey,
+                              disabledTextColor: Colors.black,
+                              padding: EdgeInsets.all(8.0),
+                              splashColor: Colors.redAccent,
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => LoginScreen()));
+                              },
+                              child: Text(
+                                S.of(context).login,
+                                style: TextStyle(fontSize: 30.0),
+                              ),
+                            ),
                           ],
                         ),
-                      ),
-                    ],
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          alignment: Alignment.bottomCenter,
+                          child: Column(
+                            children: [
+                              Text(S.of(context).developedBy,
+                                  textAlign: TextAlign.justify,
+                                  style: GoogleFonts.teko(
+                                      textStyle: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 18.0))),
+                              SizedBox(height: 1),
+                              Text(S.of(context).copyright,
+                                  style: GoogleFonts.teko(
+                                      textStyle: TextStyle(
+                                          color: Colors.black, fontSize: 18.0)))
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   )));
         });
   }
--- lib/widgets/login_screen.dart
+++ lib/widgets/login_screen.dart
@@ -15,66 +15,71 @@ class LoginScreen extends StatelessWidget {
       builder: (context, snapshot) {
         return MaterialApp(
             home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            backgroundColor: Colors.red[600],
-            title: Text(S.of(context).signIn),
-            actions: [langButton(context)],
-          ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
-                  child: TextField(
-                    autofocus: true,
-                    keyboardType: TextInputType.emailAddress,
-                    style: TextStyle(fontSize: 20.0),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                      labelText: S.of(context).email,
-                    ),
-                  )),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
-                  child: TextField(
-                    obscureText: true,
-                    style: TextStyle(fontSize: 20.0),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(5.0))),
-                      labelText: S.of(context).password,
-                    ),
-                  )),
-              SizedBox(
-                height: 50.0,
-              ),
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
+                backgroundColor: Colors.white,
+                appBar: AppBar(
+                  backgroundColor: Colors.red[600],
+                  title: Text(S.of(context).signIn),
+                  actions: [langButton(context)],
                 ),
-                color: Colors.red,
-                textColor: Colors.white,
-                disabledColor: Colors.grey,
-                disabledTextColor: Colors.black,
-                padding: EdgeInsets.all(8.0),
-                splashColor: Colors.redAccent,
-                onPressed: () {},
-                child: Text(
-                  S.of(context).enter,
-                  style: TextStyle(fontSize: 30.0),
-                ),
-              ),
-            ],
-          ),
-        ));
+                body: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 0.0),
+                          child: TextField(
+                            autofocus: true,
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(fontSize: 20.0),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0))),
+                              labelText: S.of(context).email,
+                            ),
+                          )),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 0.0),
+                          child: TextField(
+                            obscureText: true,
+                            style: TextStyle(fontSize: 20.0),
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0))),
+                              labelText: S.of(context).password,
+                            ),
+                          )),
+                      SizedBox(
+                        height: 50.0,
+                      ),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                        color: Colors.red,
+                        textColor: Colors.white,
+                        disabledColor: Colors.grey,
+                        disabledTextColor: Colors.black,
+                        padding: EdgeInsets.all(8.0),
+                        splashColor: Colors.redAccent,
+                        onPressed: () {},
+                        child: Text(
+                          S.of(context).enter,
+                          style: TextStyle(fontSize: 30.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                )));
       },
     );
   }
