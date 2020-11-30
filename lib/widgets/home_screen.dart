433de1d47b21bbfe51f955ca48c86f70131e5466
Renan Santos <renan.stos077@gmail.com>
2020-11-30 20:30:41 -0300

    basic app bar in home screen

--- lib/widgets/home_screen.dart
+++ lib/widgets/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:healthwellness/bloc/lang_bloc.dart';
+import 'package:healthwellness/generated/l10n.dart';
+
+import 'lang_button.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -12,7 +15,16 @@ class HomeScreen extends StatelessWidget {
       stream: langBloc.outLocale,
       builder: (langContext, snapshot) {
         return MaterialApp(
-          home: Scaffold(),
+          home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.red[600],
+              title: Text(S.of(mainContext).signIn),
+              actions: [langButton(mainContext)],
+            ),
+            body: SingleChildScrollView(
+              child: Column(),
+            ),
+          ),
         );
       },
     );
